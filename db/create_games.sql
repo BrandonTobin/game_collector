daf87bf75ff7085f0162af1582e4c1195c838d26
Brandon Tobin <brandontobinbusiness@gmail.com>
2023-01-27 09:45:55 -0600

    Not working but tried to follow notion as closely as possible

---
+++ db/create_games.sql
@@ -0,0 +1,23 @@
+INSERT INTO main_app_games(game, img, bio)
+  VALUES
+    ('Call of Duty: Modern Warfare II','https://assets-prd.ignimgs.com/2022/05/24/call-of-duty-modern-warfare-2-button-02-1653417394041.jpg','Blood and Gore, Intense Violence, Strong Language, Use of Drugs, Suggestive Themes, In-Game Purchases, Users Interact');
+
+INSERT INTO main_app_games(game, img, bio)
+  VALUES
+    ('Call of Duty: Modern Warfare(2019)', 'https://www.activision.com/content/dam/atvi/activision/atvi-touchui/activision/home/assets/cod-mw.jpg','Blood and Gore, Intense Violence, Strong Language, Use of Drugs, Suggestive Themes, In-Game Purchases, Users Interact');
+
+INSERT INTO main_app_games(game, img, bio)
+  VALUES
+    ('Call of Duty: Modern Warfare 2', 'https://m.media-amazon.com/images/M/MV5BNTgwYjBhYzctMzFlOC00ZTY5LTgyNTUtMjQ2YWRkZDIyODU5XkEyXkFqcGdeQXVyMTA0MTM5NjI2._V1_UX200_CR0,0,200,200_AL_.jpg','Blood and Gore, Intense Violence, Strong Language, Use of Drugs, Suggestive Themes, In-Game Purchases, Users Interact');
+
+INSERT INTO main_app_games(game, img, bio)
+  VALUES
+    ('Valorant', 'https://assets-prd.ignimgs.com/2021/12/21/valorant-1640045685890.jpg','Competitive 5v5 character-based tactical shooter');
+
+INSERT INTO main_app_games(game, img, bio)
+  VALUES
+    ('Phasmophobia', 'https://phasmophobia-game.com/images/uploads/2021-02-08/Phasmophobia-pvlsu.png','Phasmophobia game is a 4 player online co-op psychological horror where you and your team members of paranormal investigators will enter haunted locations filled with paranormal activity and gather as much evidence of the paranormal as you can.');
+
+INSERT INTO main_app_games(game, img, bio)
+  VALUES
+    ('Rocket League', 'https://avatarfiles.alphacoders.com/239/thumb-239822.png','Rocket League is a fantastical sport-based video game, developed by Psyonix (its soccer with cars));
